eeb734482f3e4ffc6f1237a35a1ff675c4e12664
phil <jaeja.phil@gmail.com>
2024-04-04 20:03:24 +0900

    feat(Jwt): Add auth to API endpoints with JWT token

---
+++ src/main/kotlin/com/inout/apiserver/config/filter/JwtAuthFilter.kt
@@ -0,0 +1,66 @@
+package com.inout.apiserver.config.filter
+
+import com.inout.apiserver.service.TokenService
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+
+@Component
+class JwtAuthFilter(
+    private val userDetailsService: UserDetailsService,
+    private val tokenService: TokenService
+) : OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val authHeader: String = request.getHeader("Authorization") ?: ""
+        // if Authorization header is not present, continue to the next filter
+        if (!authHeader.containsBearerPrefix()) {
+            filterChain.doFilter(request, response)
+            return
+        }
+
+        val token = authHeader.extractToken()
+        val email = tokenService.extractEmail(token)
+        /**
+         * if email is null, it means the token is invalid (since application signs token with email)
+         * if authentication is already set, no need to set it again
+         * - this is to prevent overriding the existing authentication
+         */
+        if (email == null || SecurityContextHolder.getContext().authentication != null) {
+            filterChain.doFilter(request, response)
+            return
+        }
+
+        val user = userDetailsService.loadUserByUsername(email)
+        // if token is valid, update security context
+        if (tokenService.isValid(token, user)) {
+            updateSecurityContext(user, request)
+        }
+
+        filterChain.doFilter(request, response)
+    }
+
+    private fun String.containsBearerPrefix(): Boolean {
+        return this.startsWith("Bearer ")
+    }
+
+    private fun String.extractToken(): String {
+        return this.substringAfter("Bearer ")
+    }
+
+    private fun updateSecurityContext(user: UserDetails, request: HttpServletRequest) {
+        val authentication = UsernamePasswordAuthenticationToken(user, null, user.authorities)
+        authentication.details = WebAuthenticationDetailsSource().buildDetails(request)
+        SecurityContextHolder.getContext().authentication = authentication
+    }
+}
--- src/main/kotlin/com/inout/apiserver/config/security/SecurityConfig.kt
+++ src/main/kotlin/com/inout/apiserver/config/security/SecurityConfig.kt
@@ -1,42 +1,71 @@
 package com.inout.apiserver.config.security
 
+import com.inout.apiserver.config.filter.JwtAuthFilter
 import com.inout.apiserver.repository.user.UserRepository
 import com.inout.apiserver.service.security.CustomUserDetailsService
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.DefaultSecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig() {
+class SecurityConfig(
+    private val userRepository: UserRepository
+) {
     @Bean
     fun securityFilterChain(
-        http: HttpSecurity
+        http: HttpSecurity,
+        jwtAuthFilter: JwtAuthFilter
     ): DefaultSecurityFilterChain {
         http
             .csrf { it.disable() }
             .authorizeHttpRequests {
                 it
-                    .requestMatchers("/**").permitAll() // TODO: temporarily allow all requests for development
-                    // .requestMatchers("/api/**").hasRole("USER") // for future use
-                    // .requestMatchers("/admin/**").hasRole("ADMIN") // for future use
+                    .requestMatchers(HttpMethod.POST, "/v1/users").permitAll() // for user creation, no authentication required
+                    .requestMatchers("/v*/auth/**").permitAll() // for user authentication, no authentication required
+                    .requestMatchers("/admin/**").hasRole("ADMIN") // admin role check
+                    .requestMatchers("/**").hasRole("USER") // user role check
             }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) } // since we are using JWT
+            .authenticationProvider(authenticationProvider())
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter::class.java)
 
         return http.build()
     }
 
     @Bean
-    fun userDetailsService(userRepository: UserRepository): UserDetailsService {
+    fun userDetailsService(): UserDetailsService {
         return CustomUserDetailsService(userRepository)
     }
 
     @Bean
     fun encoder(): PasswordEncoder {
         return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    fun authenticationProvider(): AuthenticationProvider {
+        return DaoAuthenticationProvider()
+            .apply {
+                setUserDetailsService(userDetailsService())
+                setPasswordEncoder(encoder())
+            }
+    }
+
+    @Bean
+    fun authenticationManager(config: AuthenticationConfiguration): AuthenticationManager {
+        return config.authenticationManager
     }
 }
