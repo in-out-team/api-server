89bedef1005beeaf9ed06c537f65fd8e63d6019d
phil <jaeja.phil@gmail.com>
2024-04-17 21:32:15 +0900

    test(User): Add test cases for UserEntity

--- src/test/kotlin/com/inout/apiserver/infrastructure/db/user/UserEntityTest.kt
+++ src/test/kotlin/com/inout/apiserver/infrastructure/db/user/UserEntityTest.kt
@@ -1,6 +1,7 @@
 package com.inout.apiserver.infrastructure.db.user
 
 import com.inout.apiserver.domain.user.User
+import com.inout.apiserver.domain.user.UserCreateObject
 import com.inout.apiserver.error.InOutRequireNotNullException
 import com.inout.apiserver.infrastructure.db.user.UserEntity
 import org.junit.jupiter.api.Assertions.*
@@ -54,5 +55,56 @@ class UserEntityTest {
         assertEquals(nickname, user.nickname)
         assertEquals(now, user.createdAt)
         assertEquals(now, user.updatedAt)
+    }
+
+    @Test
+    fun `of - returns UserEntity`() {
+        // given
+        val email = "test@email.com"
+        val nickname = "nickname"
+        val user = User(
+            id = 1L,
+            email = email,
+            password = "password",
+            nickname = nickname,
+            createdAt = now,
+            updatedAt = now,
+        )
+
+        // when
+        val userEntity = UserEntity.of(user)
+
+        // then
+        assertTrue(userEntity is UserEntity)
+        assertEquals(email, userEntity.email)
+        assertEquals("password", userEntity.password)
+        assertEquals(nickname, userEntity.nickname)
+        assertEquals(1L, userEntity.id)
+        assertEquals(now, userEntity.createdAt)
+        assertEquals(now, userEntity.updatedAt)
+    }
+
+    @Test
+    fun `fromCreateObject - returns UserEntity`() {
+        // given
+        val email = "test@email.com"
+        val nickname = "nickname"
+        val userCreateObject = UserCreateObject(
+            email = email,
+            password = "password",
+            nickname = nickname,
+        )
+
+        // when
+        val userEntity = UserEntity.fromCreateObject(userCreateObject)
+
+        // then
+        assertTrue(userEntity is UserEntity)
+        assertEquals(email, userEntity.email)
+        assertEquals("password", userEntity.password)
+        assertEquals(nickname, userEntity.nickname)
+        assertNull(userEntity.id)
+        assertNull(userEntity.createdAt)
+        assertNull(userEntity.updatedAt)
     }
 }
