8e5abb16133f91b9741729be9101095e17d85e38
phil <jaeja.phil@gmail.com>
2024-04-18 09:32:57 +0900

    misc(): fix import

--- src/main/kotlin/com/inout/apiserver/config/ControllerAdvice.kt
+++ src/main/kotlin/com/inout/apiserver/config/ControllerAdvice.kt
@@ -3,6 +3,7 @@ package com.inout.apiserver.config
 import com.inout.apiserver.error.*
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 
