b7de54bebf23fb8d6d577bbc40254ca8577555d9
phil <jaeja.phil@gmail.com>
2024-07-21 17:06:44 +0900

    feat(): Add RequestDto validation
    - add validation on UserController specs
    
    https://www.notion.so/SER-RequestDto-validation-468ff5ba1dee45db9ddbdf32401e3d98?pvs=4

--- src/main/kotlin/com/inout/apiserver/interfaces/web/v1/request/GoogleLoginRequest.kt
+++ src/main/kotlin/com/inout/apiserver/interfaces/web/v1/request/GoogleLoginRequest.kt
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.NotEmpty
 
 data class GoogleLoginRequest(
-    @NotEmpty
+    @field:NotEmpty
     @Schema(
         description = "구글 로그인으로부터 받은 idToken값",
         example = "abcd.efgh.ijkl",
--- src/main/kotlin/com/inout/apiserver/interfaces/web/v1/request/UpdateUserRequest.kt
+++ src/main/kotlin/com/inout/apiserver/interfaces/web/v1/request/UpdateUserRequest.kt
@@ -1,6 +1,17 @@
 package com.inout.apiserver.interfaces.web.v1.request
 
+import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.Size
+
 data class UpdateUserRequest(
+    @field:Min(value = 1, message = "아이디는 1 이상의 숫자로 입력해주세요.")
+    @Schema(description = "사용자 아이디", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
     val id: Long,
+
+    @field:NotEmpty
+    @field:Size(min = 2, max = 20, message = "닉네임은 2자 이상 20자 이하로 입력해주세요.")
+    @Schema(description = "사용자 닉네임", example = "inout", requiredMode = Schema.RequiredMode.REQUIRED)
     val nickname: String
 )
