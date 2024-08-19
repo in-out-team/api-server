c5cbe92fb852b64408591430925c938f3b5facd6
phil <jaeja.phil@gmail.com>
2024-08-20 08:31:23 +0900

    fix(): Validation 오류 & ddl auto 수정
    - @field:NotEmpty 사용 불가한 객체타입 annotate 제거
    - 사용자를 매번 생성해야 하는 번거로움이 있어 ddl auto를 update로 변경

--- app/src/main/kotlin/com/inout/apiserver/interfaces/web/v1/request/CreateWordRequest.kt
+++ app/src/main/kotlin/com/inout/apiserver/interfaces/web/v1/request/CreateWordRequest.kt
@@ -10,7 +10,6 @@ data class CreateWordRequest(
     @Schema(description = "단어 이름", example = "apple", requiredMode = Schema.RequiredMode.REQUIRED)
     val name: String,
 
-    @field:NotEmpty
     @Schema(
         description = "영한사전 기준 영어에 해당되는 값. ex) apple의 뜻을 한글로 알고싶을 경우 ENGLISH로 제공",
         example = "ENGLISH",
@@ -18,7 +17,6 @@ data class CreateWordRequest(
     )
     val fromLanguage: LanguageType,
 
-    @field:NotEmpty
     @Schema(
         description = "영한사전 기준 한국어에 해당되는 값. ex) apple의 뜻을 한글로 알고싶을 경우 KOREAN으로 제공",
         example = "KOREAN",
