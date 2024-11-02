136676c57fb8f30665a5175cb755b7f7d3a96064
Wonjun (Phillip) Choi <jaeja.phil@gmail.com>
2024-11-02 14:56:35 +0900

    feat(): 일일 학습셋 조회시 rating별 갯수 반환 (#16)
    - return metadata required for daily study set UI
    - add id field on daily study set response
    
    https://inoutedu.atlassian.net/browse/IO-17

--- app/src/main/kotlin/com/inout/apiserver/interfaces/web/v1/response/DailyStudySetResponse.kt
+++ app/src/main/kotlin/com/inout/apiserver/interfaces/web/v1/response/DailyStudySetResponse.kt
@@ -1,21 +1,47 @@
 package com.inout.apiserver.interfaces.web.v1.response
 
+import com.inout.apiserver.base.enums.FsrsCardRating
+import com.inout.apiserver.domain.study.DailyStudySet
 import com.inout.apiserver.domain.study.StudyWord
 import java.time.LocalDate
 
 data class DailyStudySetResponse(
+    val id: Long,
     val date: LocalDate,
     val studies: List<StudyWordResponse>,
+    val extraData: ExtraData,
 ) {
     companion object {
         fun of(
-            date: LocalDate,
+            dailyStudySet: DailyStudySet,
             studies: List<StudyWord>,
         ): DailyStudySetResponse {
+            val studyWordResponses = studies.map { StudyWordResponse.of(study = it.study, word = it.word) }
+            val extraData =
+                studies.map { it.study.reviewLogs }.fold(ExtraData()) { acc, reviewLogs ->
+                    when {
+                        reviewLogs.isEmpty() -> acc.copy(newCount = acc.newCount + 1)
+                        reviewLogs.last().rating == FsrsCardRating.EASY -> acc.copy(easyCount = acc.easyCount + 1)
+                        reviewLogs.last().rating == FsrsCardRating.GOOD -> acc.copy(goodCount = acc.goodCount + 1)
+                        reviewLogs.last().rating == FsrsCardRating.HARD -> acc.copy(hardCount = acc.hardCount + 1)
+                        reviewLogs.last().rating == FsrsCardRating.AGAIN -> acc.copy(againCount = acc.againCount + 1)
+                        else -> acc
+                    }
+                }
             return DailyStudySetResponse(
-                date = date,
-                studies = studies.map { StudyWordResponse.of(study = it.study, word = it.word) },
+                id = dailyStudySet.id,
+                date = dailyStudySet.date,
+                studies = studyWordResponses,
+                extraData = extraData,
             )
         }
     }
+
+    data class ExtraData(
+        val easyCount: Int = 0,
+        val goodCount: Int = 0,
+        val hardCount: Int = 0,
+        val againCount: Int = 0,
+        val newCount: Int = 0,
+    )
 }
