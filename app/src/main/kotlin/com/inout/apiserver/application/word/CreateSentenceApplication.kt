bcf479559d62d5b212a022d09c02c51287dee149
phil <jaeja.phil@gmail.com>
2025-03-15 10:27:48 +0900

    fix: Create Sentence
    - let the application run by individual word definitions through jobscheduler
      - why? too much error & retries when all transactioned together

--- app/src/main/kotlin/com/inout/apiserver/application/word/CreateSentenceApplication.kt
+++ app/src/main/kotlin/com/inout/apiserver/application/word/CreateSentenceApplication.kt
@@ -1,5 +1,6 @@
 package com.inout.apiserver.application.word
 
+import com.inout.apiserver.base.alias.WordDefinitionId
 import com.inout.apiserver.base.alias.WordId
 import com.inout.apiserver.base.enums.LexicalCategoryType
 import com.inout.apiserver.base.enums.SentenceType
@@ -8,14 +9,14 @@ import com.inout.apiserver.domain.word.SentenceCreateObject
 import com.inout.apiserver.domain.word.WordService
 import com.inout.apiserver.error.NotFoundException
 import com.inout.apiserver.infrastructure.db.word.Sentence
+import org.jobrunr.scheduling.JobScheduler
 import org.springframework.stereotype.Component
-import org.springframework.transaction.annotation.Transactional
 
 @Component
-@Transactional
 class CreateSentenceApplication(
     private val wordService: WordService,
     private val openAIService: OpenAIService,
+    private val jobScheduler: JobScheduler,
 ) {
     data class Request(
         val wordId: WordId,
@@ -27,44 +28,61 @@ class CreateSentenceApplication(
                 message = "Word not found",
                 code = "WORD_4",
             )
-        // TODO: need to bulk create sentences
-        // TODO: need to add jobrunr for async job
+
         word.definitions.forEach { wordDefinition ->
-            wordService.getSentencesByWordDefinitionId(wordDefinition.id!!).let {
-                if (it.isNotEmpty()) return@forEach
-            }
+            jobScheduler.enqueue { createSentencesFor(word.id!!, wordDefinition.id!!) }
+        }
+    }
 
-            val openAIWordDefinitionSentenceResponse =
-                openAIService.fetchWordDefinitionSentence(
-                    word = word.name,
-                    meaning = wordDefinition.meaning,
-                    fromLanguage =
-                        word.fromLanguage.name
-                            .lowercase()
-                            .replaceFirstChar { it.uppercase() },
-                    toLanguage =
-                        word.toLanguage.name
-                            .lowercase()
-                            .replaceFirstChar { it.uppercase() },
+    fun createSentencesFor(
+        wordId: WordId,
+        wordDefinitionId: WordDefinitionId,
+    ) {
+        wordService.getSentencesByWordDefinitionId(wordDefinitionId).let {
+            if (it.isNotEmpty()) return
+        }
+
+        val word =
+            wordService.getWordById(wordId) ?: throw NotFoundException(
+                message = "Word not found",
+                code = "WORD_4",
+            )
+        val wordDefinition =
+            word.definitions.find { it.id == wordDefinitionId } ?: throw NotFoundException(
+                message = "Word definition not found",
+                code = "WORD_4",
+            )
+
+        val openAIWordDefinitionSentenceResponse =
+            openAIService.fetchWordDefinitionSentence(
+                word = word.name,
+                meaning = wordDefinition.meaning,
+                fromLanguage =
+                    word.fromLanguage.name
+                        .lowercase()
+                        .replaceFirstChar { it.uppercase() },
+                toLanguage =
+                    word.toLanguage.name
+                        .lowercase()
+                        .replaceFirstChar { it.uppercase() },
+            )
+
+        openAIWordDefinitionSentenceResponse.sentences.forEachIndexed { index, sentence ->
+            val sentenceCreateObject =
+                SentenceCreateObject(
+                    wordDefinitionId = wordDefinitionId,
+                    type = if (index % 2 == 0) SentenceType.READING else SentenceType.WRITING,
+                    content = sentence.content,
+                    translation = sentence.translation,
+                    lexicalCategories =
+                        sentence.lexicalCategories.map { lexicalCategoryMap ->
+                            Sentence.LexicalCategoryInfo(
+                                word = lexicalCategoryMap.word,
+                                lexicalCategory = LexicalCategoryType.of(lexicalCategoryMap.lexicalCategory),
+                            )
+                        },
                 )
-
-            openAIWordDefinitionSentenceResponse.sentences.forEachIndexed { index, sentence ->
-                val sentenceCreateObject =
-                    SentenceCreateObject(
-                        wordDefinitionId = wordDefinition.id,
-                        type = if (index % 2 == 0) SentenceType.READING else SentenceType.WRITING,
-                        content = sentence.content,
-                        translation = sentence.translation,
-                        lexicalCategories =
-                            sentence.lexicalCategories.map { lexicalCategoryMap ->
-                                Sentence.LexicalCategoryInfo(
-                                    word = lexicalCategoryMap.word,
-                                    lexicalCategory = LexicalCategoryType.of(lexicalCategoryMap.lexicalCategory),
-                                )
-                            },
-                    )
-                wordService.createSentence(sentenceCreateObject)
-            }
+            wordService.createSentence(sentenceCreateObject)
         }
     }
 }
