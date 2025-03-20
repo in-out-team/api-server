31341e5f7013cc06ab03aa28f6ef5560ee7c8287
phil <jaeja.phil@gmail.com>
2025-03-20 22:58:57 +0900

    feature: Add Dictionary Validations
    - remove migrated codes
    
    https://inoutedu.atlassian.net/browse/IO-60

--- app/src/main/kotlin/com/inout/apiserver/base/service/openai/OpenAIService.kt
+++ app/src/main/kotlin/com/inout/apiserver/base/service/openai/OpenAIService.kt
@@ -4,8 +4,6 @@ import com.aallam.openai.api.chat.TextContent
 import com.aallam.openai.client.OpenAI
 import com.inout.apiserver.base.enums.AiVoiceType
 import com.inout.apiserver.base.enums.SenderType
-import com.inout.apiserver.base.service.openai.dto.Definition
-import com.inout.apiserver.base.service.openai.dto.OpenAIWordDefinitionResponse
 import com.inout.apiserver.base.service.openai.dto.OpenAIWordDefinitionSentenceResponse
 import com.inout.apiserver.base.service.openai.dto.OpenAIWritingSentenceFeedbackResponse
 import com.inout.apiserver.base.service.openai.provider.OpenAIRequestProvider
@@ -18,99 +16,6 @@ class OpenAIService(
     private val openAIRequestProvider: OpenAIRequestProvider,
     private val openai: OpenAI,
 ) {
-    fun fetchWordDefinition(
-        word: String,
-        fromLanguage: String = "English",
-        toLanguage: String = "Korean",
-    ): OpenAIWordDefinitionResponse {
-        val chatCompletionRequest = openAIRequestProvider.genWordInfoRequest(word, fromLanguage, toLanguage)
-        val response = runBlocking { openai.chatCompletion(chatCompletionRequest) }
-
-        /**
-         * TODO: log response
-         *   ex: token usage, caller info
-         *
-         * response format: ChatCompletion.class
-         *   - docs: https://platform.openai.com/docs/api-reference/chat/object
-         * {
-         *   "id": "chatcmpl-9YPHOqmrcrv86cQL79X7nMEy88xkC",
-         *   "created": 1717987354,
-         *   "model": "gpt-3.5-turbo-0125",
-         *   "usage": { // Usage.class
-         *     "promptTokens": 420,
-         *     "completionTokens": 92,
-         *     "totalTokens": 512
-         *   },
-         *   "choices": [ // List<ChatChoice.class>
-         *       {
-         *         "index": 0,
-         *         "logpobs": null,
-         *         "finishReason": "stop", // stop | length | content_filter | tool_calls
-         *         "message": {
-         *           "role": "assistant",
-         *           "messageContent": { // TextContent.class
-         *             "content": { // Content.class
-         *               {
-         *                 "definitions": [
-         *                   {
-         *                     "type": "noun",
-         *                     "definition": "바나나",
-         *                     "preContext": "과일로 먹는 식품"
-         *                   },
-         *                   {
-         *                     "type": "noun",
-         *                     "definition": "바나나 나무",
-         *                     "preContext": "열대지방에서 자라는 나무"
-         *                   }
-         *                 ]
-         *               }
-         *             }
-         *           }
-         *         }
-         *       }
-         *   ]
-         * }
-         */
-        val chatMessageContent =
-            response.choices
-                .first()
-                .message.messageContent as TextContent
-
-        return OpenAIWordDefinitionResponse.fromJson(chatMessageContent.content)
-    }
-
-    /**
-     * TODO: need to run this in a non-blocking way (parallel processing)
-     */
-    fun validateAndTrimWordDefinition(
-        word: String,
-        fromLanguage: String,
-        toLanguage: String,
-        definitions: List<Triple<String, String, String>>, // type, definition, preContext
-    ): OpenAIWordDefinitionResponse {
-        val chatCompletionRequest =
-            openAIRequestProvider.genValidateAndTrimWordDefinitionRequest(
-                word = word,
-                fromLanguage = fromLanguage,
-                toLanguage = toLanguage,
-                definitions =
-                    definitions.map {
-                        Definition(
-                            type = it.first,
-                            definition = it.second,
-                            preContext = it.third,
-                        )
-                    },
-            )
-        val response = runBlocking { openai.chatCompletion(chatCompletionRequest) }
-        val chatMessageContent =
-            response.choices
-                .first()
-                .message.messageContent as TextContent
-
-        return OpenAIWordDefinitionResponse.fromJson(chatMessageContent.content)
-    }
-
     fun fetchWordDefinitionSentence(
         word: String,
         meaning: String,
