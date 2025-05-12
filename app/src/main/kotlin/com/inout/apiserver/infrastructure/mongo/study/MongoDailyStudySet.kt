a32fd653470a3e34b0c18d35271378db7ae0a738
phil <jaeja.phil@gmail.com>
2025-05-12 14:36:53 -0400

    feat: Migrate to Mongo
    - add user, refresh token, study, and daily study set mongo documents & repositories
    
    https://inoutedu.atlassian.net/browse/IO-76

---
+++ app/src/main/kotlin/com/inout/apiserver/infrastructure/mongo/study/MongoDailyStudySet.kt
@@ -0,0 +1,44 @@
+package com.inout.apiserver.infrastructure.mongo.study
+
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.mongodb.core.index.CompoundIndex
+import org.springframework.data.mongodb.core.index.CompoundIndexes
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.Instant
+import java.time.LocalDate
+
+@Document(collection = "daily_study_sets")
+@CompoundIndexes(
+    CompoundIndex(
+        name = "unique_user_id_date",
+        def = "{'user_id': 1, 'date': 1}",
+        unique = true,
+    ),
+)
+data class MongoDailyStudySet(
+    @Id
+    val id: ObjectId? = null,
+    val userId: ObjectId,
+    val date: LocalDate,
+    val studyIds: List<ObjectId> = emptyList(),
+    @CreatedDate
+    val createdAt: Instant? = null,
+    @LastModifiedDate
+    val updatedAt: Instant? = null,
+) {
+    companion object {
+        // FIXME: temporary
+        fun fromCreateObject(
+            userId: ObjectId,
+            date: LocalDate,
+        ): MongoDailyStudySet =
+            MongoDailyStudySet(
+                userId = userId,
+                date = date,
+                studyIds = emptyList(),
+            )
+    }
+}
---
+++ app/src/main/kotlin/com/inout/apiserver/infrastructure/mongo/user/MongoRefreshToken.kt
@@ -0,0 +1,23 @@
+package com.inout.apiserver.infrastructure.mongo.user
+
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.mongodb.core.index.Indexed
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.Instant
+
+@Document(collection = "refresh_tokens")
+data class MongoRefreshToken(
+    @Id
+    val id: ObjectId? = null,
+    val userId: ObjectId,
+    @Indexed(unique = true)
+    val token: String,
+    val expiresAt: Instant,
+    @CreatedDate
+    val createdAt: Instant? = null,
+    @LastModifiedDate
+    val updatedAt: Instant? = null,
+)
